c76ff7e3510df47e5b07093a52cc838c95f3506a
XiaGary <garyxia10@gmail.com>
2024-09-07 00:50:56 +1200

    Added UI component to my rock-paper-scissors game

--- script.js
+++ script.js
@@ -8,45 +8,64 @@ function getComputerChoice(){
         return "scissors"
     }
 };
-getComputerChoice()
 
-let humanChoice;
-function getHumanChoice(){
-    humanChoice = prompt("Pick rock, paper, or scissors!").toLowerCase()
-    return humanChoice;
-};
+let humanScore = 0;
+let computerScore = 0;
 
-let humanSelection;
-let computerSelection;
-let humanScore;
-let computerScore;
 
-function playGame(nTimes){
-        
-    humanScore = 0;
-    computerScore = 0;
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const results = document.querySelector("#results");
+rock.addEventListener("click", () => {
+    playRound("rock", getComputerChoice())
+});
+paper.addEventListener("click", () => {
+    playRound("paper", getComputerChoice())
+});
+scissors.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice())
+});
 
-    for(let i = 1; i <= nTimes; i++){
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper"){
+        humanScore += 1;
+        results.textContent = `You win this round! Your score is ${humanScore}, computer's score is ${computerScore}.`
+    } else if(humanChoice === computerChoice){
+        results.textContent = `You tied this round! Your score is ${humanScore}, computer's score is ${computerScore}.`
+    } else{
+        computerScore += 1
+        results.textContent = `You lose this round! Your score is ${humanScore}, computer's score is ${computerScore}.`
     }
-    function playRound(humanChoice, computerChoice){
 
-        if(humanChoice === 'rock' && computerChoice === 'scissors' || humanChoice === 'paper' && computerChoice === 'rock' || humanChoice === 'scissors' && computerChoice === 'paper'){
-            humanScore++
-            console.log(`You win! Your ${humanChoice} beats the computers ${computerChoice}.`) 
-            console.log(`Your score is: ${humanScore}, computer score is: ${computerScore}.`);
-        } else if(humanChoice === computerChoice){
-            console.log(`You tied! You both picked ${humanChoice}.`)
-            console.log(`Your scoe is: ${humanScore}, computer score is: ${computerScore}.`);
-        } else{
-            computerScore++
-            console.log(`You lose! Computer's ${computerChoice} beats your ${humanChoice}.`);
-            console.log(`Your score is: ${humanScore}, computer score is: ${computerScore}.`);
-        }
-    }  
-    
+    if(humanScore === 5){
+        results.textContent = `You have won this match!`
+    } else if(computerScore === 5){
+        results.textContent = `You have lost this match!`
+    }
 }
 
-playGame(5)
+
+    
+    
+    // if(humanChoice === 'rock' && computerChoice === 'scissors' || humanChoice === 'paper' && computerChoice === 'rock' || humanChoice === 'scissors' && computerChoice === 'paper'){
+    //     humanScore++
+    //     console.log(`You win! Your ${humanChoice} beats the computers ${computerChoice}.`) 
+    //     console.log(`Your score is: ${humanScore}, computer score is: ${computerScore}.`);
+    // } else if(humanChoice === computerChoice){
+    //     console.log(`You tied! You both picked ${humanChoice}.`)
+    //     console.log(`Your scoe is: ${humanScore}, computer score is: ${computerScore}.`);
+    // } else{
+    //     computerScore++
+    //     console.log(`You lose! Computer's ${computerChoice} beats your ${humanChoice}.`);
+    //     console.log(`Your score is: ${humanScore}, computer score is: ${computerScore}.`);
+    // }
+    
+    
+    
+    
+    // for(let i = 1; i <= nTimes; i++){
+    //     humanSelection = getHumanChoice();
+    //     computerSelection = getComputerChoice();
+    //     playRound(humanSelection, computerSelection);
+    // }
